0c6b6add187e2150d9c80b0c06f307a50e13399c
natashastramrood <n.w.s@stramrood.com>
2025-10-15 07:58:48 -0400

    Created a new character class and world code

---
+++ basegame.py
@@ -0,0 +1,29 @@
+import pygame
+#from characterclass import Character
+
+#user = Character()
+
+pygame.init()
+WIDTH = 600
+HEIGHT = 400
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+print(type(screen))
+clock = pygame.time.Clock()
+running = True
+
+
+sky_blue = [0, 150, 255]
+
+while running: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    #user.update() #update user position
+    screen.fill(sky_blue)
+    #user.draw(screen) #paint the user on top of the blue background
+    pygame.display.flip() #show the person playing the game
+
+    clock.tick(60)  
+
+pygame.quit()
