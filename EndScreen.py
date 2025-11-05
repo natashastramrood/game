08111354b6800d2f78acad10a02670f9fefe2d7e
natashastramrood <n.w.s@stramrood.com>
2025-11-05 08:50:03 -0500

    changed end screen

--- EndScreen.py
+++ EndScreen.py
@@ -1,25 +1,32 @@
 import pygame
+from background import SpaceBackground
+
 def runEndScreen(result):
     WIDTH = 1100
     HEIGHT = 700
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    font = pygame.font.Font(None, 200)
+    font = pygame.font.Font('text/good timing bd.otf', 150)
+    bright_blue = (47, 147, 201)
     running = True
     clock = pygame.time.Clock()
+
+    background = SpaceBackground(WIDTH, HEIGHT)
+    background = background.get_background()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.fill((0, 0, 0))  # clear screen with black
+        screen.blit(background, (0,0))  # space background
         if result == 0: 
-            text_surface = font.render("You Died!", True, (255, 255, 255))
+            text_surface = font.render("You Died!", True, bright_blue)
         elif result == 1:
-            text_surface = font.render("You Win!", True, (255, 255, 255))
+            text_surface = font.render("You Win!", True, bright_blue)
         else: 
-            text_surface = font.render("End Game!", True, (255, 255, 255))
+            text_surface = font.render("End Game!", True, bright_blue)
         
-        screen.blit(text_surface, (100, 100))
+        screen.blit(text_surface, (150, 200))
         pygame.display.flip()
         clock.tick(1200)  
     
